ea49a060fc83086527e21c3d4bfee3271ae621a8
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2026-02-06 22:03:12 +0100

    migrated api functions to class ExternalServices

---
+++ src/scripts/modules/ExternalServices.js
@@ -0,0 +1,52 @@
+export default class ExternalServices {
+  constructor() {
+    this.gamerPowerURL = "https://gamerpower.p.rapidapi.com/api/";
+    this.cheapSharkURL = "https://www.cheapshark.com/api/1.0/";
+    this.options = {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": import.meta.env.VITE_RAPIDAPI_KEY,
+        "x-rapidapi-host": "gamerpower.p.rapidapi.com",
+      },
+    };
+  }
+
+  async getData(url, options = undefined) {
+    try {
+      const response = await fetch(url, options);
+
+      if (!response.ok) {
+        throw new Error(`HTTP Error: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async getGiveaways() {
+    const giveaways = await getData(
+      `${this.gamerPowerURL}giveaways`,
+      this.options,
+    );
+    return giveaways;
+  }
+
+  async getDeals(limit = 60) {
+    const deals = await getData(`${this.cheapSharkURL}deals?pageSize=${limit}`);
+    return deals;
+  }
+
+  async searchDeals(term) {
+    const deals = await getData(`${this.cheapSharkURL}games?title=${term}`);
+    return deals;
+  }
+
+  async getRandomDeal() {
+    const randomNumber = Math.floor(Math.random() * 11);
+    const deals = this.getDeals(10);
+    return deals[randomNumber];
+  }
+}
